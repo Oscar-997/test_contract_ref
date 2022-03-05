3b35b345d910677574e9c6adbe733b1c97a40dd4
hoangtinh997 <hoangtinhqk@gmail.com>
2022-03-05 08:12:18 +0700

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,29 @@
-use std::collections::HashMap;
 use near_sdk::collections::{ UnorderedMap, LookupMap};
-use near_sdk::{AccountId, Balance, env, near_bindgen, BorshStorageKey };
-use near_sdk::json_types::{ValidAccountId, U128};
+use near_sdk::{AccountId, Balance, env, near_bindgen, BorshStorageKey, StorageUsage };
+use near_sdk::json_types::{ValidAccountId};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+
+const U128_STORAGE: StorageUsage = 16;
+const U64_STORAGE: StorageUsage = 8;
+const U32_STORAGE: StorageUsage = 4;
+/// max length of account id is 64 bytes. We charge per byte.
+const ACC_ID_STORAGE: StorageUsage = 64;
+/// As a key, 4 bytes length would be added to the head
+const ACC_ID_AS_KEY_STORAGE: StorageUsage = ACC_ID_STORAGE + 4;
+const KEY_PREFIX_ACC: StorageUsage = 64;
+/// As a near_sdk::collection key, 1 byte for prefiex
+const ACC_ID_AS_CLT_KEY_STORAGE: StorageUsage = ACC_ID_AS_KEY_STORAGE + 1;
+pub const ERR11_INSUFFICIENT_STORAGE: &str = "E11: insufficient $NEAR storage deposit";
+
+// ACC_ID: the Contract accounts map key length
+// + VAccount enum: 1 byte
+// + U128_STORAGE: near_amount storage
+// + U32_STORAGE: legacy_tokens HashMap length
+// + U32_STORAGE: tokens HashMap length
+// + U64_STORAGE: storage_used
+pub const INIT_ACCOUNT_STORAGE: StorageUsage =
+    ACC_ID_AS_CLT_KEY_STORAGE + 1 + U128_STORAGE + U32_STORAGE + U32_STORAGE + U64_STORAGE;
+
 
 #[derive(BorshStorageKey, BorshSerialize)]
 pub(crate) enum StorageKey {
@@ -41,6 +62,24 @@ impl Account {
             }
         }
     }
+
+    // [AUDIT_01]
+    /// Returns amount of $NEAR necessary to cover storage used by this data structure.
+    pub fn storage_usage(&self) -> Balance {
+        (INIT_ACCOUNT_STORAGE + 
+            self.tokens.len() as u64 * (KEY_PREFIX_ACC + ACC_ID_AS_KEY_STORAGE + U128_STORAGE)
+        ) as u128
+            * env::storage_byte_cost()
+    }
+
+    /// Asserts there is sufficient amount of $NEAR to cover storage usage.
+    pub fn assert_storage_usage(&self) {
+        assert!(
+            self.storage_usage() <= self.near_amount,
+            "{}",
+            ERR11_INSUFFICIENT_STORAGE
+        );
+    }
 }
 #[near_bindgen]
 pub struct Contract {
@@ -56,13 +95,14 @@ impl Contract {
         let sender_id = env::predecessor_account_id();
         let mut account = self.internal_unwrap_account(&sender_id);
         account.register(&token_ids);
-
+        self.internal_save_account(&sender_id, account);
     }
 }
 
 impl Contract {
     pub fn internal_save_account(&mut self, account_id: &AccountId, account: Account) {
-        
+        account.assert_storage_usage();
+        self.accounts.insert(&account_id, &account.into());
     }
 
     pub fn internal_get_account(&self, account_id: &AccountId) -> Option<Account> {
